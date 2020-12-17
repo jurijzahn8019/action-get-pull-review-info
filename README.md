2b94d4d4d08bec14b117d8db50e2bc101a029338
Jurij Zahn <jurij.zahn@otto.de>
2020-12-17 12:38:36 +0100

    feat(action): first implementation

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
 # action-get-pull-review-info
-Action to retrieve review data for a pull request
+
+Action to retrieve review data for a pull request heavily inspired by:
+<https://github.com/jrylan/github-action-reviews-counter>
+
+The differences are:
+
+- Allows pass inputs instead of using event data
+- Ouputs more outputs
+- Allows definition of Allowed Author Associations to calculate the counts
+
+## Inputs
+
+### `token`
+
+**Required** Github PAT with organization scope, e.g. secrets.GH_TOKEN
+
+### `owner`
+
+The organization/user owns the repository, default from event data
+
+### `number`
+
+Pull request number to look for, default from event data
+
+### `repo`
+
+the repo name (reponame in the owner scope without owner path)
+where the pull request is located, default from event data
+
+### `collaborators`
+
+This can be one or multiple of: 'COLLABORATOR', 'CONTRIBUTOR', 'FIRST_TIME_CONTRIBUTOR', 'FIRST_TIMER', 'MEMBER', 'OWNER', 'NONE'
+
+Multiple values are to be separated by `,` (comma). For example: `COLLABORATOR,CONTRIBUTOR`, Defaults to All possible
+
+This can be used to perform extra calculations only for given collaborator associations
+
+## Outputs
+
+### `requested`
+
+Count of the Reamining Review Requestes of the Pull Reqeuest.
+This will be 0 when all requested Peoples (Teams) have given their
+Reviews
+
+### `reviews`
+
+Count of the given reviews in all states.
+
+### `approved`
+
+Overal Count of the Reviews with Approvals
+
+### `changes_requested`
+
+Overal Count of the Reviews wich Requests changes
+
+### `commented`
+
+Overal Count of the Reviews with only comments
+
+### `dismissed`
+
+Overal Count of the Dismissed Reviews
+
+### `pending`
+
+Overal Count of the Pending Reviews
+
+### `<state>_<association>`
+
+These outputs will contain calculated amount of reviews by Status and Author Association
+
+for example: `approved_collaborator` or `changes_requested_first_time_contributor`
+
+Usefull for more detailed info
+
+### `<state>_collaborators`
+
+This will contain amount of reviews by state for `Allowed Collaborators`
+which is defined by `collaborators` input. If no collaborators specified will equal to the overal counts. For Example: `approved_collaborators`, or `commented_collaborators`
+
+## Example usage
+
+```yaml
+name: User was Assigned to Issue
+on:
+  issues: [opened, reopened]
+
+jobs:
+  do-something:
+    steps:
+      # Pass Inputs directly
+      - uses: jurijzahn8019/action-get-pull-review-info@v0.0.1
+        id: checker
+        with:
+          token: ${{ secrets.GH_TOKEN }}
+          owner: ${{ github.repository_owner }}
+          repo: ${{ github.event.repository.name }}
+          number: ${{ github.event.issue.number }}
+
+      # Use Event Data
+      - uses: jurijzahn8019/action-get-pull-review-info@v0.0.1
+        id: checker
+        with:
+          token: ${{ secrets.GH_TOKEN }}
+
+      - name: Do Something if all reviews were given
+        if: ${{ steps.checker.outputs.requested < 1 }}
+        run: |
+          do something with the pull request
+```
+
+## Test run
+
+```bash
+DEBUG_DEPTH=8 DEBUG=action-get-pull-review-info* \
+  INPUT_TOKEN=$GITHUB_TOKEN \
+  INPUT_OWNER="my-org" \
+  INPUT_REPO="my-repo"
+  INPUT_NUMBER=666 \
+  ts-node src/index.ts
+
+DEBUG_DEPTH=8 DEBUG=action-get-pull-review-info* \
+  INPUT_TOKEN=$GITHUB_TOKEN \
+  GITHUB_EVENT_PATH=fixtures/review.submitted.event.json \
+  ts-node src/index.ts
+```
---
+++ src/index.ts
@@ -0,0 +1,203 @@
+/* eslint-disable camelcase */
+
+import { getInput, setFailed, setOutput, info } from "@actions/core";
+import { getOctokit } from "@actions/github";
+import { promises as fs } from "fs";
+import debug from "debug";
+
+const dbg = debug("action-get-pull-review-info:index:debug");
+const trace = debug("action-get-pull-review-info:index:trace");
+
+export enum ReviewState {
+  APPROVED = "APPROVED",
+  CHANGES_REQUESTED = "CHANGES_REQUESTED",
+  COMMENTED = "COMMENTED",
+  DISMISSED = "DISMISSED",
+  PENDING = "PENDING",
+}
+
+export enum CommentAuthorAssociation {
+  COLLABORATOR = "COLLABORATOR",
+  CONTRIBUTOR = "CONTRIBUTOR",
+  FIRST_TIME_CONTRIBUTOR = "FIRST_TIME_CONTRIBUTOR",
+  FIRST_TIMER = "FIRST_TIMER",
+  MEMBER = "MEMBER",
+  OWNER = "OWNER",
+  NONE = "NONE",
+}
+
+interface GqlReview {
+  author: {
+    name: string;
+  };
+  updatedAt: string;
+  authorAssociation: CommentAuthorAssociation;
+  state: ReviewState;
+}
+
+interface GqlResult {
+  repository: {
+    pullRequest: {
+      reviewRequests?: {
+        nodes: {
+          requestedReviewer: { name: string };
+        }[];
+      };
+      reviews: {
+        nodes: GqlReview[];
+      };
+    };
+  };
+}
+
+export async function run(): Promise<void> {
+  dbg("Retrieve pull request info");
+  const { GITHUB_EVENT_PATH } = process.env;
+  let event:
+    | undefined
+    | {
+        pull_request: {
+          number: number;
+        };
+        repository: {
+          name: string;
+          owner: {
+            login: string;
+          };
+        };
+      };
+  try {
+    event = JSON.parse(await fs.readFile(GITHUB_EVENT_PATH as string, "utf8"));
+  } catch {
+    event = undefined;
+  }
+
+  try {
+    dbg("Retrieve inputs");
+    const token = getInput("token", { required: true });
+    const owner =
+      getInput("owner", { required: false }) || event?.repository.owner.login;
+    const number_input = getInput("number", { required: false });
+    const repo =
+      getInput("repo", { required: false }) || event?.repository.name;
+    const number = number_input
+      ? Number.parseInt(number_input, 10)
+      : event?.pull_request.number;
+    const collaborators_input = getInput("collaborators");
+    const collaborators_array = collaborators_input
+      ? collaborators_input.split(",").map((i) => i.trim())
+      : [];
+    const collaborators =
+      collaborators_array.length > 0
+        ? Object.values(CommentAuthorAssociation).filter((a) =>
+            collaborators_input.includes(a)
+          )
+        : Object.values(CommentAuthorAssociation);
+
+    dbg("Inputs: %s, %s, %d, %s", owner, repo, number, collaborators);
+
+    if (!owner || !repo || !number) {
+      throw new Error("Failed to retrieve required parameters");
+    }
+
+    const client = getOctokit(token);
+
+    dbg("Build query");
+    const query = `
+      query ($owner: String!, $repo: String!, $number: Int!) {
+        repository(owner: $owner, name: $repo) {
+          pullRequest(number: $number) {
+            reviewRequests(first: 50) {
+              nodes {
+                requestedReviewer {
+                  ... on User { name: login }
+                  ... on Team { name }
+                }
+              }
+            }
+            reviews(first: 50) {
+              nodes {
+                author { name: login }
+                updatedAt
+                authorAssociation
+                state
+              }
+            }
+          }
+        }
+      }    
+    `;
+
+    dbg("Fetch data");
+    const params = { query, owner, number, repo };
+    trace("Params: %O", params);
+    const data = await client.graphql<GqlResult>(params);
+    trace("Data: %O", data);
+
+    const requested = data.repository.pullRequest.reviewRequests?.nodes || [];
+    const reviews = (data.repository.pullRequest.reviews?.nodes || []).map(
+      (r) => ({
+        ...r,
+        updatedAt: Date.parse(r.updatedAt),
+      })
+    );
+
+    const byName: Record<string, typeof reviews> = {};
+    reviews.forEach((r) => {
+      // Only perocess reviews if the author was not rerequested
+      if (
+        !requested.find((rq) => rq.requestedReviewer.name === r.author.name)
+      ) {
+        if (!byName[r.author.name]) {
+          byName[r.author.name] = [];
+        }
+        byName[r.author.name].push(r);
+      }
+    });
+
+    const lasts: typeof reviews = [];
+    Object.entries(byName).forEach(([, revs]) => {
+      lasts.push(revs.sort((a, b) => a.updatedAt - b.updatedAt)[0]);
+    });
+    trace("Last Reviews Per Author: %O", lasts);
+
+    const res: Record<string, Omit<GqlReview, "updatedAt">[]> = {};
+    Object.values(ReviewState).forEach((s) => {
+      res[s.toLowerCase()] = lasts.filter((l) => l.state === s);
+    });
+
+    dbg("Process Result");
+    trace("Result Data: %O", res);
+
+    info("Pull request info");
+
+    info(`Reviews Given: ${lasts.length}`);
+    setOutput("reviews", lasts.length);
+    info(`Requested Remaining: ${requested.length}`);
+    setOutput("requested", requested.length);
+
+    Object.entries(res).forEach(([state, revs]) => {
+      info(`${state}: ${requested.length}`);
+      setOutput(state, revs.length);
+
+      Object.values(CommentAuthorAssociation).forEach((type) => {
+        const count = revs.filter((r) => r.authorAssociation === type).length;
+
+        setOutput(`${state}_${type.toLowerCase()}`, count);
+      });
+
+      const bycols = revs.filter((r) =>
+        collaborators.includes(r.authorAssociation)
+      ).length;
+
+      info(`${state} for Allowed Collaborators: ${requested.length}`);
+      setOutput(`${state}_collaborators`, bycols);
+    });
+    info("Done");
+  } catch (e) {
+    dbg("Failed:", e);
+    setFailed(e.message);
+  }
+}
+
+export default run();
